c1cd0feb2d0548ed14c23d10b5573f193e8f2297
a.chigozie <shesgottadevelopit@gmail.com>
2017-05-14 04:37:13 +0100

    added more styled components to the page

--- content-excerpt.php
+++ content-excerpt.php
@@ -7,8 +7,13 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header style="text-align:center;">
 		<?php the_title( '<h2 class="article-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );?>
-		<div class="article-info">
-		<span class="article-date"><?php the_date(); ?></span>
+		<div class="article-meta">
+		<span class="article-date"><?php echo get_the_date("l // F j, Y"); ?></span>
+		<div class="article-tags">
+			<?php echo get_the_tag_list('<span class="article-tag">','</span><span class="article-tag">','</span>'); ?>
+			<?php //echo get_tags(); ?>
+			<?php //the_tags(); ?>
+		</div>
 	</div>
 	</header>
 
